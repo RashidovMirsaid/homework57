9ffd8b3acb5e4fe2351e549004527dd2eacae7f3
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2024-01-19 04:04:50 +0600

    добален компонент UserItem который будет показывать пользователей которые зарегестрировались

---
+++ src/components/UserItem/UserItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { User } from '../../types';
+
+const UserItem: React.FC<User> = ({ name, email, active, role }) => {
+	return (
+		<div className='card mb-3'>
+			<div className='card-body'>
+				<p className='card-title'>{name}</p>
+				<p className='card-title'>email {email}</p>
+				<p className='card-title'>active {active}</p>
+				<p className='card-title'>role {role}</p>
+			</div>
+		</div>
+	);
+};
+
+export default UserItem;
