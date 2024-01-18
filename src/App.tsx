9d0cd2dea90953a872edbf30723e73c5727aea19
Mirsaid Rashidov <mirsaidrasidov1@gmail.com>
2024-01-19 04:21:16 +0600

    добавлены импорты компонентов и добавлен функционал добавоения пользователя

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import UserForm from './components/UserForm/UserForm';
+import UserItem from './components/UserItem/UserItem';
+import { User } from './types';
+import Users from './components/Users/Users';
 
 function App() {
+	const [users, setUsers] = useState<User[]>([]);
+
+	const addUser = (user: User) => {
+		setUsers([...users, user]);
+	};
 	return (
 		<>
-			<UserForm />
+			<div className='container row'>
+				<div className='col-md-6'>
+					<UserForm addUser={addUser} />
+				</div>
+				<div className='col-md-6'>
+					<Users users={users} />
+				</div>
+			</div>
 		</>
 	);
 }
